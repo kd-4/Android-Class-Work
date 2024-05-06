154561c061ad43ddb73bea938bb334d565cedc16
kd-4 <161795625+kd-4@users.noreply.github.com>
2024-05-06 22:08:11 +0800

    Update index.kt

--- 期中考试/index.kt
+++ 期中考试/index.kt
@@ -1,27 +1,70 @@
+点点.:
 package com.example.middleexam
 
-import android.annotation.SuppressLint
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Scaffold
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.example.middleexam.ui.theme.MiddleExamTheme
 
-@OptIn(ExperimentalMaterial3Api::class)
-@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
-@Composable
-fun IndexPage(teachers: List<Teacher>, students: List<Student>) {
-    val navController = rememberNavController()
-    Scaffold(
-        topBar = { TopAppBar(title = { Text("Index Page") }) }
-    ) {
-        NavHost(navController, startDestination = "pageA") {
-            composable("pageA") { PageA(teachers) }
-            composable("pageB") { PageB(students) }
-            composable("pageC") { PageC() }
-        }
-    }
+class MainActivity : ComponentActivity() {
+override fun onCreate(savedInstanceState: Bundle?) {
+super.onCreate(savedInstanceState)
+setContent {
+MiddleExamTheme {
 }
+}
+}
+}
+@Composable
+fun Navigator(){
+// val navController = rememberNavController()
+val navController = rememberNavController()
+NavHost(navController = navController, startDestination = "Home"){
+composable("Home"){ HomePage(navController)}
+composable("A"){ PageA(navController)}
+composable("B"){ PageB(navController)}
+composable("C"){ PageC(navController)}
+
+
+}
+}
+
+@Composable
+fun HomePage(navController: NavController){
+Column {
+Button(onClick = {
+navController.navigate("A")
+}) {
+Text(text="A")
+}
+Button(onClick = {
+navController.navigate("B")
+}) {
+Text(text="B")
+}
+Button(onClick = {
+navController.navigate("C")
+}) {
+Text(text="C")
+}
+}
+}
+
+
+@Composable
+@Preview(showSystemUi = true)
+fun PrevieHost(){
+Navigator()
+}
+
+点点.:
+然后才跳出来这个
